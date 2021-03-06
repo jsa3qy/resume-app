990553d2d65d5a163ce56dea5f5d42d0e34fe54e
Jesse Alloy <jalloy@yext.com>
2021-03-05 22:49:09 -0500

    gallary work

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import Photography from './Components/Gallary';
 
 class App extends Component {
 
@@ -48,10 +46,8 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
+        <Photography/>
         <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
---
+++ src/Components/Gallary.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
+
+export default class Photography extends React.Component {
+  render() {
+    return (
+      <div>
+        <Gallery photos={PHOTO_SET} />
+        <div className="hidden-xs">
+          <Gallery photos={PHOTO_SET_2} />
+        </div>
+        <div className="hidden-xs">
+          <Gallery photos={PHOTO_SET_3} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const PHOTO_SET = [
+    {
+      src: './images/favoriteShots/teton-yosemite.jpg',
+      width: 4914,
+      height: 3275
+    },
+    {
+      src: './images/favoriteShots/house-mountain.jpg',
+      width: 5813,
+      height: 3236
+    },
+    {
+      src: './images/favoriteShots/butt-cave.jpg',
+      width: 2947,
+      height: 3703
+    },
+    {
+      src: './images/favoriteShots/zion-sunset.jpg',
+      width: 4596,
+      height: 3064
+    }
+  ];
+
+  const PHOTO_SET_2 = [
+    {
+      src: './images/favoriteShots/az-pandora.jpg',
+      width: 6000,
+      height: 4000
+    },
+    {
+      src: './images/favoriteShots/river-sunrise.jpg',
+      width: 5463,
+      height: 3693
+    },
+    {
+      src: './images/favoriteShots/snowy-trees.jpg',
+      width: 3752,
+      height: 3678
+    },
+    {
+      src: './images/favoriteShots/zion-bump.jpg',
+      width: 6000,
+      height: 4000
+    },
+    {
+      src: './images/favoriteShots/zion-side.jpg',
+      width: 3206,
+      height: 3599
+    }
+  ];
+
+  const PHOTO_SET_3 = [
+    {
+      src: './images/favoriteShots/lake-rock.jpg',
+      width: 6000,
+      height: 4000
+    },
+    {
+      src: './images/favoriteShots/foggy-landscape.jpg',
+      width: 5845,
+      height: 3654
+    },
+    {
+      src: './images/favoriteShots/grass-shoot-sunset.jpg',
+      width: 6000,
+      height: 4000
+    },
+    {
+      src: './images/favoriteShots/pink-moon.jpg',
+      width: 6000,
+      height: 4000
+    },
+    {
+      src: './images/favoriteShots/river-canyon.jpg',
+      width: 5485,
+      height: 3999
+    }
+  ]
